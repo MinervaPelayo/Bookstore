d0180e5017b92c2617e6b55963dffbb6a90189e8
Minerva Pelayo <a1800737@myy.haaga-helia.fi>
2019-01-31 22:57:13 +0200

    First commit

---
+++ src/main/java/fi/haagahelia/bookstore/web/BookController.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class BookController {
+	@GetMapping("/index")
+	
+	
+}
