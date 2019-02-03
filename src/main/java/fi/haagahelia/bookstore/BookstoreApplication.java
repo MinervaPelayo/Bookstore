401caca9b61268a8071306fe436e48017be76641
Minerva Pelayo <a1800737@myy.haaga-helia.fi>
2019-02-03 19:55:25 +0200

    Database and JPA exercises

--- src/main/java/fi/haagahelia/bookstore/BookstoreApplication.java
+++ src/main/java/fi/haagahelia/bookstore/BookstoreApplication.java
@@ -1,14 +1,38 @@
 package fi.haagahelia.bookstore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import fi.haagahelia.bookstore.domain.Book;
+import fi.haagahelia.bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
-}
+	@Bean
+	public CommandLineRunner bookDemo(BookRepository repository) {
+		return (args) -> {
+			repository.save(new Book("A Tale of Two Cities", "Charles Dickens", 1998, 9780486406510L, 6.00));
+			repository.save(new Book("The Lord of the Rings", "J.R.R. Tolkien", 2005, 9780544003415L, 17.49));
+			repository.save(new Book("The Little Prince", "Antoine de Saint-Exupery", 2013, 9788478886296L, 6.95));
+			repository.save(new Book("The Da Vinci Code", "Dan Brown", 2009, 9780385504201L, 8.99));
+			repository.save(new Book("The Catcher in the Rye", "J.D. Salinger", 1991, 9788420674209L, 7.19));
+			repository.save(new Book("One Hundred Years of Solitude", "Gabriel Garcia Marquez", 2006, 9780241971826L, 11.39));
+			repository.save(new Book("When Breath Becomes Air", "Paul Kalanithi", 2016, 9781784701994L, 15.41));
 
+			for (Book book : repository.findAll()) {
+				log.info(book.toString());
+			}
+		};
+	}
+
+}
--- src/main/java/fi/haagahelia/bookstore/domain/Book.java
+++ src/main/java/fi/haagahelia/bookstore/domain/Book.java
@@ -1,11 +1,32 @@
 package fi.haagahelia.bookstore.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Book {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
 	private String title;
 	private String author;
 	private int year;
-	private long isbn;
+	private Long isbn;
 	private double price;
+
+	public Book() {
+	}
+
+	public Book(String title, String author, int year, Long isbn, double price) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.year = year;
+		this.isbn = isbn;
+		this.price = price;
+	}
 
 	public String getTitle() {
 		return title;
@@ -46,4 +67,11 @@ public class Book {
 	public void setPrice(double price) {
 		this.price = price;
 	}
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", title=" + title + ", author=" + author + ", year=" + year + ", isbn=" + isbn
+				+ ", price=" + price + "]";
+	}
+
 }
