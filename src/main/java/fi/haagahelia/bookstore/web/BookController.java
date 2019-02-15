f073c7d6a4862c3b40a7cf39eb53bb624129110f
Minerva Pelayo <a1800737@myy.haaga-helia.fi>
2019-02-15 13:06:50 +0200

    REST

--- src/main/java/fi/haagahelia/bookstore/web/BookController.java
+++ src/main/java/fi/haagahelia/bookstore/web/BookController.java
@@ -1,11 +1,15 @@
 package fi.haagahelia.bookstore.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import fi.haagahelia.bookstore.domain.Book;
 import fi.haagahelia.bookstore.domain.BookRepository;
@@ -13,7 +17,6 @@ import fi.haagahelia.bookstore.domain.CategoryRepository;
 
 @Controller
 public class BookController {
-
 	@Autowired
 	private BookRepository repository;
 	@Autowired
@@ -49,5 +52,15 @@ public class BookController {
 		model.addAttribute("book", repository.findById(bookId));
 		model.addAttribute("categories", crepository.findAll());
 		return "editbook";
+	}
+	
+	@RequestMapping(value="/books", method = RequestMethod.GET)
+	public @ResponseBody List<Book> bookListRest(){
+		return(List<Book>) repository.findAll();
+	}
+	
+	@RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+	public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId){
+		return repository.findById(bookId);
 	}
 }
